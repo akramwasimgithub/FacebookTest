c354b4f9a575df1abdc2da0a08f135ba3525f68b
akramwasimgithub <imwasim322@gmail.com>
2022-03-09 22:03:21 +0530

    Added: class to take screenshot and save it

--- src/test/java/com/bridelabz/Screenshot.java
+++ src/test/java/com/bridelabz/Screenshot.java
@@ -1,2 +1,39 @@
-package com.bridelabz;public class Screenshot {
+package com.bridelabz;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.io.FileHandler;
+import org.testng.annotations.Test;
+
+
+import java.io.File;
+import java.io.IOException;
+
+public class Screenshot {
+
+
+    @Test
+    public void captureScreenshot() throws InterruptedException {
+
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\63not\\selenium\\chromedriver_win32 (1)\\chromedriver.exe");
+        WebDriver driver =new ChromeDriver();
+        driver.get("https://www.facebook.com/");
+        driver.manage().window().maximize();
+        Thread.sleep(2000);
+
+        TakesScreenshot screenshot = (TakesScreenshot)  driver;
+        File srcFile = screenshot.getScreenshotAs(OutputType.FILE);
+        //FileHandler.copy(srcFile, new File ( ."\\screenshot\\"+ System.currentTimeMillis()/1000 + "_facebook.jpg"));
+        File desFile = new File(".\\screenshot\\" + System.currentTimeMillis() / 1000 + "_facebook.png");
+
+        try {
+            FileHandler.copy(srcFile, desFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
 }
